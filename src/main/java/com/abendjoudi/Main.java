1ebe048e7d09b17f32b3f4d113b0d1b7ea3e460f
abendjoudi <bendjoudiali@gmail.com>
2016-12-13 18:19:00 -0500

    Initial commit

---
+++ src/main/java/com/abendjoudi/Main.java
@@ -0,0 +1,11 @@
+package com.abendjoudi;
+
+/**
+ * Created by abendjoudi on 12/13/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
